6dd4f89a8f35b1e8962b9057afd3d24534ac350c
Michael Tao <mtao@dgp.toronto.edu>
2019-10-05 12:32:33 -0400

    fangda init

---
+++ README.md
@@ -0,0 +1,1 @@
+# LosTopos
---
+++ src/common/tunicate/interval.cpp
@@ -0,0 +1,14 @@
+// ---------------------------------------------------------
+//
+//  interval.cpp
+//  Tyson Brochu 2011
+//
+//  Function definitions for switching rounding modes.
+//
+// ---------------------------------------------------------
+
+#include "LosTopos/common/tunicate/interval.h"
+
+int Interval::s_previous_rounding_mode = ~0;
+
+
